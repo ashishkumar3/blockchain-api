12f4ca8d3740731aeeea1408ad2aa6983fc39d8b
Ashish Kumar <ashishk4371@gmail.com>
2019-01-12 10:51:28 +0530

    chain validation and replacement added with testing

--- block.js
+++ block.js
@@ -1,28 +1,28 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 
-class Block{
-  constructor(timestamp, lasthash, hash, data){
+class Block {
+  constructor(timestamp, lasthash, hash, data) {
     this.timestamp = timestamp;
     this.lastHash = lasthash;
     this.hash = hash;
     this.data = data;
   }
 
-  toString(){
+  toString() {
     return `Block-
       Timestamp: ${this.timestamp}
-      Last Hash: ${this.lasthash.substring(0, 10)}
-      Hash: ${this.hash.substring(0, 10)}
-      Data: ${this.data}
+      Last Hash: ${this.lastHash.substring(0, 10)}
+      Hash:      ${this.hash.substring(0, 10)}
+      Data:      ${this.data}
     `;
   }
 
   // To start with the first block.
-  static genesis(){
-    return new this('Genesis Time', '-----', 'b31k-3k12b', []);
+  static genesis() {
+    return new this("Genesis Time", "-----", "f1r57-h45h", []);
   }
 
-  static mineBlock(lastBlock, data){
+  static mineBlock(lastBlock, data) {
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
     const hash = Block.hash(timestamp, lastHash, data);
@@ -30,8 +30,14 @@ class Block{
   }
 
   // create hash for every block created using sha256
-  static hash(timestamp, lastHash, data){
+  static hash(timestamp, lastHash, data) {
     return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
+
+  static blockHash(block) {
+    const { timestamp, lastHash, data } = block;
+
+    return Block.hash(timestamp, lastHash, data);
   }
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -1,24 +1,51 @@
-const Blockchain = require('./blockchain');
-const Block = require('./block');
+const Blockchain = require("./blockchain");
+const Block = require("./block");
 
-describe('blockchain', () => {
-  
-  let bc;
+describe("blockchain", () => {
+  let bc, bc2;
 
   beforeEach(() => {
     bc = new Blockchain();
+    bc2 = new Blockchain();
   });
 
-
-  it('starts with the genesis block', () => {
+  it("starts with the genesis block", () => {
     expect(bc.chain[0]).toEqual(Block.genesis());
   });
 
-  it('adds a new block', () => {
-    const data = 'randomdata';
+  it("adds a new block", () => {
+    const data = "randomdata";
     bc.addBlock(data);
 
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
   });
 
+  it("validates a valid chain", () => {
+    bc2.addBlock("bar");
+    expect(bc.isValidChain(bc2.chain)).toBe(true);
+  });
+
+  it("invalidatesa chain with corrupt genesis block", () => {
+    bc2.chain[0].data = "bad data";
+
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+
+  it("invalidates a corrupt chain", () => {
+    bc2.addBlock("foo");
+    bc2.chain[1].data = "not foo";
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+
+  it("replaces the blockchain with a new chain", () => {
+    bc2.addBlock("yoyo");
+    bc.replaceChain(bc2.chain);
+    expect(bc.chain).toEqual(bc2.chain);
+  });
+
+  it("does not replace the chain if new chain is less than or equal to the blockchain in length", () => {
+    bc.addBlock("test block");
+    bc.replaceChain(bc2.chain);
+    expect(bc.chain).not.toEqual(bc2.chain);
+  });
 });
